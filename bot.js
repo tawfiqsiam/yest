0da1ed52c46c0630cd9cd7065fc4103702b13772
tawfiqsiam <toto.siam999@gmail.com>
2018-12-22 17:40:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3280,6 +3280,17 @@ client.on("guildMemberAdd", msg => {
 
 
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('+star')){
+if (message.author.id !== '521479557375852547') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
