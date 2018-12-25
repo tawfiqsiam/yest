cdad31c9539ffc3e9cf6d534f87255998c66a983
tawfiqsiam <toto.siam999@gmail.com>
2018-12-25 14:40:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1000,7 +1000,7 @@ if(ra3d.content.startsWith(prefix + 'ccolors')) {
 
 client.on('message', message => {
 var prefix = "!";
-var cats = ["https://www.reddit.com/r/memes/,https://www.reddit.com/r/memes/comments/a9ehhp/cant_even_argue/,https://i.imgur.com/CCDj00U.jpg,https://imgur.com/gallery/GQ8By,https://imgur.com/gallery/szFIw,https://imgur.com/gallery/RbUeY"]
+var cats = ["https://www.reddit.com/r/memes/","https://www.reddit.com/r/memes/comments/a9ehhp/cant_even_argue/","https://i.imgur.com/CCDj00U.jpg","https://imgur.com/gallery/GQ8By","https://imgur.com/gallery/szFIw,https://imgur.com/gallery/RbUeY"]
         var args = message.content.split(" ").slice(1);
     if(message.content.startsWith(prefix + 'memes')) {
          var cat = new Discord.RichEmbed()
