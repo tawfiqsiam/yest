54eeb81e3cfec4b2df7215a32b8daae038b3bd95
tawfiqsiam <toto.siam999@gmail.com>
2018-12-26 17:33:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -398,10 +398,11 @@ client.user.setStatus("dnd")
 
 
 
+  
   client.on('message', msg => {
     if(msg.author.bot) return;
     
-    if(msg.content === '.star') {
+    if(msg.content === '!star') {
       client.guilds.forEach(g => {
         
         let l = g.id
@@ -421,7 +422,6 @@ client.user.setStatus("dnd")
     }
     
   })
-
 
 
   
