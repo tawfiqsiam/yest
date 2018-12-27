af6020af33a846a2e63c6dfbd258ea3142c39789
tawfiqsiam <toto.siam999@gmail.com>
2018-12-27 11:56:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1551,7 +1551,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` .help | ${client.guilds.size} servers,"http://twitch.tv/S-F")
+client.user.setGame(` .help | ${client.guilds.size} servers,"http://twitch.tv/S-F"`)
 client.user.setStatus("dnd")
 });
 
